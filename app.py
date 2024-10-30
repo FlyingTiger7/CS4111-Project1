e4d8999272fb537f4350f614ceddeb85f92b0997
flyingtiger7 <robinhwang7@gmail.com>
2024-10-30 15:20:50 -0400

    Added test tables from our database to see if they actually work

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
-from flask import Flask, render_template
+import os
+from sqlalchemy import *
+from sqlalchemy.pool import NullPool
+from flask import Flask, request, render_template, g, redirect, Response
 
 app = Flask(__name__)
+
+
+DB_USER = "ah4100"
+DB_PASSWORD = "03220322"
+
+DB_SERVER = "w4111.cisxo09blonu.us-east-1.rds.amazonaws.com"
+
+DATABASEURI = "postgresql://"+DB_USER+":"+DB_PASSWORD+"@"+DB_SERVER+"/proj1part2"
+
+engine = create_engine(DATABASEURI)
+
+
+
+@app.route('/tables')
+def list_tables():
+    with engine.connect() as connection:
+        # Query to list tables owned by ah4100
+        result = connection.execute(text("""
+            SELECT tablename 
+            FROM pg_catalog.pg_tables 
+            WHERE tableowner = 'ah4100'
+        """))
+        tables = [row['tablename'] for row in result]
+
+    return render_template('tables.html', tables=tables)
 
 @app.route('/')
 def home():
